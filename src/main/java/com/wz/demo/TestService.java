ae45275585b4ad6e916e22fcec6134a467d45a8f
wuzhen <1033797878@qq.com>
2020-09-11 09:12:24 +0800

    ...

--- src/main/java/com/wz/demo/TestService.java
+++ src/main/java/com/wz/demo/TestService.java
@@ -4,9 +4,40 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class TestService {
     public static void main(String[] args) {
+
+    }
+
+    private static void testSet() {
+        Set<Integer> set = new LinkedHashSet<>();
+//        Set<Integer> set = new TreeSet<>();
+        set.add(1);
+        set.add(null);
+        set.add(null);
+        set.add(1);
+        set.add(2);
+
+        System.out.println(set);
+    }
+
+    private static void testFinally() {
+        try {
+            System.out.println("I'm the inner of try");
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("ha ha ha");
+        }
+    }
+
+    private static void testURL() {
         try {
             URL url = new URL("https://www.zkong.com");
             InputStream inputStream = url.openStream();
@@ -20,6 +51,5 @@ public class TestService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
